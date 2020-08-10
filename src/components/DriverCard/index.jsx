7003850c39267d5627a853c2f367ad13c4e17542
Alessandro Amarri <amarri.alessandro@gmail.com>
2020-08-11 00:14:23 +0100

    Iteration 7

--- src/components/DriverCard/index.jsx
+++ src/components/DriverCard/index.jsx
@@ -3,12 +3,13 @@ import './style.css';
 import Rating from './../../components/Rating';
 
 const DriverCardComponent = (props) => {
+  console.log(props.rating);
   return (
     <div className="DriverCard">
       <img src={props.img} alt="" />
       <div className="DriverDetails">
         <h3>{props.name}</h3>
-        <Rating rating={props.rating} />
+        <Rating>{props.rating}</Rating>
         <p>
           {props.car.model} - {props.car.licensePlate}
         </p>
